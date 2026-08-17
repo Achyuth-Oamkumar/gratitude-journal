f049addd456408c48830da09ebac5174490af66a
achyuth <oamkumarachyuth@gmail.com>
2026-08-17 22:00:01 +0530

    Remove venv and unused legacy app folders from remote

--- gratitude_app/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class GratitudeAppConfig(AppConfig):
-    name = 'gratitude_app'
--- journal/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-from .models import Entry
-
-class EntrySerializer(serializers.ModelSerializer):
-    owner = serializers.ReadOnlyField(source='owner.username')
-
-    class Meta:
-        model = Entry
-        fields = ['id', 'owner', 'date', 'content', 'created_at', 'updated_at']
